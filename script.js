3dcd240d2b8ce2900c9df1dc8cfb2bcd9c41a716
seunayoola <zcaboay@ucl.ac.uk>
2023-03-28 17:08:08 +0100

    making changes

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-let filter_btn = document.querySelectorAll('.filter-btn');
-let tab_items = document.querySelectorAll('.tab-item');
-
-for (let i = 0; i < filter_btn.length; i++) {
-  filter_btn[i].addEventListener('click', function () {
-    for (let j = 0; j < filter_btn.length; j++) {
-      filter_btn[j].classList.remove('active');
-    }
-    let select_tab = filter_btn[i].getAttribute('data-tab');
-    filter_btn[i].classList.add('active');
-    for (let t = 0; t < tab_items.length; t++) {
-      document.querySelector('.tab-filter-item-container').style.height =
-        tab_items[t].scrollHeight + 'px';
-      if (tab_items[t].classList.contains(select_tab)) {
-        tab_items[t].classList.add('select_tab');
-      } else {
-        tab_items[t].classList.remove('select_tab');
-      }
-    }
-  });
-}
-
-for (let th = 0; th < tab_items.length; th++) {
-  document.querySelector('.tab-filter-item-container').style.height =
-    tab_items[th].scrollHeight + 'px';
-}
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
